6a007ec7ecec40f9f7e3aa7afca73ecd42deb085
Oupman Miralles <oupman.miralles@qmr.cat>
2026-02-19 02:21:44 +0100

    Updated setup_system to use .env and feedback format updated

--- scripts/setup_system.sh
+++ scripts/setup_system.sh
@@ -2,28 +2,47 @@
 
 set -e  # Exit on error
 
-# Define Error message on Exit on error
-failure() {
-    echo "Error ocurred on line $1"
-    echo "Exiting setup..."
-}
-trap 'failure $LINENO' ERR
+# Colors
+R='\033[0;31m' 
+G='\033[0;32m' 
+Y='\033[1;33m' 
+B='\033[0;34m' 
+NC='\033[0m'
+
+# Messages Formats
+log() { echo -e "${B}[$(date +%T)]${NC} $1\n"; }
+error() { echo -e "${R}[ERROR]${NC} $1\n"; exit 1; }
+info() { echo -e "${B}[INFO]${NC} $1\n"; }
+success() { echo -e "${G}[SUCCESS]${NC} $1\n"; }
+warning() { echo -e "${Y}[WARNING]${NC} $1\n"; }
 
 
+# Loading .env params
+ENV_FILE=".env"
+if [ -f "$ENV_FILE" ]; then
+    log "Sourcing $ENV_FILE..."
+    source "$ENV_FILE"
+else
+    log "No .env found, using internal defaults."
+fi
 
-echo -n "\n--- STARTING SETUP ---\n\n" 
+VM_NAME=${VM_NAME:-debian-gsx}
+VM_USER=${VM_USER:-gsx}
+VM_PASS=${VM_PASS:-admin}
 
 # Ensure script is run as root
 if [ "$EUID" -ne 0 ]; then
-    echo "This script must be run as root"
-    echo "Usage: su -c ./setup_system.sh"
-    exit 1
+    warning "This script must be run as root"
+    error "Usage: su -c ./setup_system.sh"
 fi
+
+log "\n\t--- STARTING SETUP ---" 
 
 # Test internet connection
 if ! ping -c 1 8.8.8.8 &>/dev/null; then
-    echo "No internet connection."
-    exit 1
+    error "No internet connection."
+else
+    info "Internet connection established"
 fi
 
 # Install the required packages
@@ -35,10 +54,10 @@ apt update >/dev/null
 for pkg in "${PACKAGES[@]}"; do
     # -W (show) -f (fromat: Status) to check if already installed
     if dpkg-query -W -f='${Status}' "$pkg" 2>dev/null | grep -q "ok installed"; then
-        echo -e "$pkg is already installed. Skipping...\n"
+        warning "$pkg is already installed. Skipping..."
     else
-        echo -e "Installing $pkg...\n"
-        apt install -y "$pkg" &>/dev/null || { echo -n "Failed to install $pkg.\n"; exit 1; }
+        log "Installing $pkg..."
+        apt install -y "$pkg" &>/dev/null || error "Failed to install $pkg."
     fi
 done
 
@@ -46,37 +65,29 @@ done
 #   Give user all permisions on its own config file
 #   Host = ( AsAnyUser : AnyGroup) Sudo
 #   Hidden file => .rw-r--r--
-USER="gsx"
+
+# USER="gsx"
+USER="$VM_USER"
 
 if id -nG "$USER" | grep -qw "sudo"; then
-    echo -e "User $USER is already in the sudo group.\n"
+    warning "User $USER is already in the sudo group."
 else
     echo "$USER ALL=(ALL:ALL) ALL" > /etc/sudoers.d/$USER
-    chmod 0440 /etc/sudoers.d/gsx
-    echo -e "User $USER added to the sudo group.\n"
+    chmod 0440 /etc/sudoers.d/$USER
+    log "User $USER added to the sudo group."
 fi
 
 # Create Admin Directory
 #   Only admins group can write on this directory (rwx-rwx-r-x)
-DIR="/opt/gsx-admin"
-echo -e "Creating Admin directory...\n"
+DIR="/opt/$USER-admin"
+info -e "Creating Admin directory..."
 mkdir -p "$DIR"/{scripts,backups,configs}
 chown -R root:sudo "$DIR"
 chmod -R 775 "$DIR"
-echo -e "Admin directory created: $DIR\n"
-
-# # Verify
-# echo -e "--- ACTIONS PERFORMED ---\n"
-# echo -e "1. Packages:\n"
-# for pkg in "${PACKAGES[@]}"; do
-#     dpkg -l | grep "^ii  $pkg"
-# done
-# echo -e "\nUser $USER" id
-# echo -e "\nAdmin directory <$DIR> with permisions:"
-# ls -ld "$DIR"
+log -e "Admin directory created: $DIR"
 
 # Lock the root password for security
-echo -e "\nLocking Root password...\n"
+info "\nLocking Root password..."
 sudo passwd -l root
 
-echo -n "\t--- SETUP COMPLETED ---" 
+success "\t--- SETUP COMPLETED ---" 
